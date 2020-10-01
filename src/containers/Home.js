d1666f0a83946301f160429c053493d5730f35f0
Fabiha Ahmed <fsafabiha@gmail.com>
2020-10-01 15:31:37 -0400

    filler info

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import Header from "../components/Header";
+
+const weatherKey = `67498b5b8ab3f81914de08af4d299a7a`;
+
 function Home() {
+  /* Display
+    Weather Type (ex. Cloudy)
+    Current Temperature
+    High Temperature
+    Low Temperature
+    Cloudiness
+    Humidity
+    Wind Speed
+    */
   return (
-    <div>
-      <h1>Weather App</h1>
-    </div>
+    <>
+      <Header />
+      <main className="Home">
+        <h2>Weather in Seoul</h2>
+        <div className="WeatherInfo">
+          <p>Weather Type: Cloudy</p>
+          <p>Current Temperature: 100 degrees</p>
+          <p>High Temperature: 100 degrees</p>
+          <p>Low Temperature: 80 degrees</p>
+          <p>Cloudiness: 100</p>
+          <p>Humidity: 35%</p>
+          <p>Wind Speed: 3km/h</p>
+        </div>
+      </main>
+    </>
   );
 }
 
